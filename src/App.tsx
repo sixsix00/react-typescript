688767ac278edc9427e2c4ae0f9da13a26e526cb
Lucas <s35985456@hotmail.com>
2024-03-17 23:36:00 +0800

    調整位置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-import { Button } from 'antd'
+import { Button, Tabs, Space } from 'antd'
 import TableList from './components/TableList'
+import type { TabsProps } from 'antd';
+interface DataType {
+  key: number;
+  districtCode: string;
+  alias: string;
+  created: string;
+  edited: string;
+  actions: string[];
+}
+const datas: DataType[] = [
+  {
+      key: 1,
+      districtCode: "7777",
+      alias: "555",
+      created: "2024-02-15",
+      edited: "2024-02-15",
+      actions: ["edit","delete"],
+  },
+  {
+      key: 2,
+      districtCode: "7777",
+      alias: "555",
+      created: "2024-02-15",
+      edited: "2024-02-15",
+      actions: ["edit","delete"],
+  },
+  {
+      key: 3,
+      districtCode: "7777",
+      alias: "555",
+      created: "2024-02-15",
+      edited: "2024-02-15",
+      actions: ["edit","delete"],
+  }
+]
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +49,23 @@ function App() {
     setCount(0);
   }
 
+  const onChange = (key: string) => {
+    console.log(key);
+  };
+  
+  const items: TabsProps['items'] = [
+    {
+      key: '1',
+      label: 'Alias',
+      children: <TableList datas={datas}></TableList>,
+    },
+    {
+      key: '2',
+      label: 'District',
+      children: 'Content of Tab Pane 2',
+    },
+  ];
+
   return (
     <>
       <Button>新增資訊</Button>
@@ -22,7 +74,7 @@ function App() {
         <Button onClick={addValue}>按我吧</Button>
         <Button onClick={resetValue}>Reset</Button>
       </div>
-      <TableList></TableList>
+      <Tabs centered={true} defaultActiveKey="1" items={items} onChange={onChange} />
     </>
   )
 }
--- src/components/TableList.tsx
+++ src/components/TableList.tsx
@@ -1,83 +1,66 @@
 import type { TableProps } from 'antd';
 import { Table, Space, Button } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-
 interface DataType {
-    key: number;
-    districtCode: string;
-    alias: string;
-    created: string;
-    edited: string;
-    actions: string[];
+  key: number;
+  districtCode: string;
+  alias: string;
+  created: string;
+  edited: string;
+  actions: string[];
 }
-const datas: DataType[] = [
-    {
-        key: 1,
-        districtCode: "7777",
-        alias: "555",
-        created: "2024-02-15",
-        edited: "2024-02-15",
-        actions: ["edit","delete"],
-    },
-    {
-        key: 2,
-        districtCode: "7777",
-        alias: "555",
-        created: "2024-02-15",
-        edited: "2024-02-15",
-        actions: ["edit","delete"],
-    },
-    {
-        key: 3,
-        districtCode: "7777",
-        alias: "555",
-        created: "2024-02-15",
-        edited: "2024-02-15",
-        actions: ["edit","delete"],
-    }
-]
 
-const cols: TableProps<DataType>['columns'] = [
-    {
-      title: 'District Code',
-      dataIndex: 'districtCode',
-      key: 'districtCode',
-    },
-    {
-      title: 'Alias',
-      dataIndex: 'alias',
-      key: 'alias',
-    },
-    {
-      title: 'Created',
-      dataIndex: 'created',
-      key: 'created',
-    },
-    {
-      title: 'Edited',
-      dataIndex: 'edited',
-      key: 'edited',
-    },
-    {
-      title: 'Actions',
-      dataIndex: 'actions',
-      key: 'actions',
-      render: (_, { actions }) => (
-        <Space size="middle">
-          {
-            actions.map((v) => {
-                if(v === 'delete') {
-                    return <Button  shape="circle" icon={<DeleteOutlined />} />
-                }
-                if(v === 'edit') {
-                    return <Button shape="circle" icon={<EditOutlined />} />
-                }
+// interface DataProps {
+//   datas: DataType[];
+// }
 
-            })
-          }
-        </Space>
-      ),
-    },
-]
+const TableList = ({datas}: {datas: DataType[]}) => {
+  
 
-export default () => <Table columns={cols} dataSource={datas}></Table>
+  const cols: TableProps<DataType>['columns'] = [
+      {
+        title: 'District Code',
+        dataIndex: 'districtCode',
+        key: 'districtCode',
+      },
+      {
+        title: 'Alias',
+        dataIndex: 'alias',
+        key: 'alias',
+      },
+      {
+        title: 'Created',
+        dataIndex: 'created',
+        key: 'created',
+      },
+      {
+        title: 'Edited',
+        dataIndex: 'edited',
+        key: 'edited',
+      },
+      {
+        title: 'Actions',
+        dataIndex: 'actions',
+        key: 'actions',
+        render: (_, { actions }) => (
+          <Space size="middle">
+            {
+              actions.map((v) => {
+                  if(v === 'delete') {
+                      return <Button  shape="circle" icon={<DeleteOutlined />} />
+                  }
+                  if(v === 'edit') {
+                      return <Button shape="circle" icon={<EditOutlined />} />
+                  }
+
+              })
+            }
+          </Space>
+        ),
+      },
+  ]
+
+  return <Table columns={cols} dataSource={datas}></Table>
+}
+
+export default TableList
